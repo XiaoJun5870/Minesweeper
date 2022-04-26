1a24651654f2c7c8f4b94076438cff8719581c57
XiaoJun5870 <101238691+XiaoJun5870@users.noreply.github.com>
2022-04-26 17:32:55 +0800

    1
    檔案

---
+++ Rummikub/Rummikub/card.h
@@ -0,0 +1,13 @@
+#include "rule.h"
+#ifndef card_h
+#define card_h
+class Card {
+public:
+
+private:
+	int red;
+	int blue;
+	int black;
+	int orange;
+};
+#endif
---
+++ Rummikub/Rummikub/rule.h
@@ -0,0 +1,9 @@
+#ifndef Rule_h
+#define Rule_h
+class Rule {
+public:
+
+private:
+
+};
+#endif
