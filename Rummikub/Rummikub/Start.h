e21a4999dd75405394b99d17e1c255d9a3ff2bf1
Kenji486 <101238652+Kenji486@users.noreply.github.com>
2022-06-02 12:49:07 +0800

    Update Start.h

--- Rummikub/Rummikub/Start.h
+++ Rummikub/Rummikub/Start.h
@@ -1,1 +1,14 @@
-#pragma once
+#ifndef Start_h
+#define Start_h
+#include "card.h"
+class Start {
+private:
+	int first;
+	card hc_arr[14];
+public:
+	void setFirst(int);
+	void setHc(card);
+	int getFirst();
+	int getHc();
+};
+#endif
