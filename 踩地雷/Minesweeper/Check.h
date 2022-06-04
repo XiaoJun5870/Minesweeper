b043126fdaf609797c5d6e3b91182d94bd6a0a9c
Kenji486 <101238652+Kenji486@users.noreply.github.com>
2022-06-04 22:39:49 +0800

    Update Check.h

--- 踩地雷/Minesweeper/Check.h
+++ 踩地雷/Minesweeper/Check.h
@@ -4,9 +4,15 @@
 class Check
 {
 public:
-
+	void Start();
+	bool End();
 private:
-　
+	int map[100];                               /*地雷盤面*/
+	bool mine[100];                         /*偵測是否採到地雷用*/
+	inline int Position(int x, int y) {                /*位子函數*/
+		return 10 * x + y;
+	}
+
 };
 
 #endif
