f4511ea5938ee17f7105e955574ca8a58fe73bcc
XiaoJun <101238691+XiaoJun5870@users.noreply.github.com>
2022-06-07 22:16:52 +0800

    Update Game.h

--- 踩地雷/Minesweeper/Game.h
+++ 踩地雷/Minesweeper/Game.h
@@ -12,9 +12,9 @@ public:
 	void Touch();
 
 private:
-	int map[100];                               /*地雷盤面*/
-	bool mine[100];                         /*偵測是否採到地雷用*/
-	inline int Position(int x, int y) {                /*位子函數*/
+	int map[100];/*地雷盤面*/
+	bool mine[100];/*偵測是否採到地雷用*/
+	inline int Position(int x, int y) {/*位子函數*/
 		return 10 * x + y;
 	}
 
