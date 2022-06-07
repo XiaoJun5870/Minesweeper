2e8e83a180984f60a037debd2640f7c91d631ef6
XiaoJun <101238691+XiaoJun5870@users.noreply.github.com>
2022-06-07 22:15:42 +0800

    Update Start.cpp

--- 踩地雷/Minesweeper/Start.cpp
+++ 踩地雷/Minesweeper/Start.cpp
@@ -4,23 +4,23 @@
 #include "Game.h"
 #include "Start.h"
 using namespace std;
-void Start::start() {                                 /*開始遊戲*/
+void Start::start() {/*開始遊戲*/
     Game c;
-    c.Initially();
+    c.Initially();/*初始化盤面*/
     bool notboom = true;
-    while (notboom) {
-        c.Show(' ');
-        c.Touch();
-        notboom = c.check();
+    while (notboom) {/*沒有地雷*/
+        c.Show(' ');/*空氣*/
+        c.Touch();/*踩*/
+        notboom = c.check();/*沒有地雷->檢查*/
     }
     return;
 }
 
-bool Start::end() {                                   /*結束遊戲*/
+bool Start::end() {/*結束遊戲*/
     char answer;
-    cout << "Do you want to play again?(y/n)";
+    cout << "Do you want to play again?(y/n)";/*在玩一次?*/
     cin >> answer;
-    if (answer == 'y' || answer == 'Y') {
+    if (answer == 'y' || answer == 'Y') {/*【是/否】*/
         system("cls");
         return true;
     }
